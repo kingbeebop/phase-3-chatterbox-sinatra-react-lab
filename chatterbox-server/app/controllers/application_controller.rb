561234269888a55dff601a16f2f54e60c566a387
Brendan Ward <kingbeebop@gmail.com>
2022-07-12 11:15:48 -0400

    commit tree 3a94561b36b85d037f89b6b0e083a3eff806e7dc

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,24 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    Message.all.order(:created_at).to_json
+  end
   
+  post '/messages' do
+    Message.create(
+      body: params[:body],
+      username: params[:username]
+    ).to_json
+  end
+
+  patch '/messages/:id' do
+    Message.find(params[:id]).update(
+      body: params[:body]
+    ).to_json
+  end
+
+  delete 'messages/:id' do
+    Message.find(params[:id]).destroy.to_json
+  end
 end
